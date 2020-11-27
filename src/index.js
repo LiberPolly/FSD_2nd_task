86072b6715f7ac02ff5ab3cf22356c3706eeeddd
Alina Galochkina <bashkirovaalina@gmail.com>
2020-11-27 16:12:52 +0300

    Webpack setup made

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './theme/style.css';
+// import * as $ from 'jquery';
+// import keys from 'lodash/keys';
+// import json from './assets/p.json';
+
+console.log('smth');
+// console.log(json);
+
+// $('pre').html(JSON.stringify(json));
---
+++ webpack.config.js
@@ -0,0 +1,170 @@
+const path = require('path');
+const fs = require('fs');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const postcss = require('postcss');
+const doiuse = require('doiuse');
+const autoprefixer = require('autoprefixer');
+const stylelint = require('stylelint');
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+console.log(`isDev: ${isDev}; isProd: ${isProd}`);
+
+const pages = [];
+
+fs
+  .readdirSync(path.resolve(__dirname, 'src', 'pages'))
+  .forEach((file) => {
+    pages.push(file.split('/', 2));
+  });
+
+const htmlPlugins = pages.map(fileName => new HTMLWebpackPlugin({
+  filename: `${fileName}.html`,
+  template: `./pages/${fileName}/${fileName}.pug`,
+  minify: {
+    collapseWhitespace: isProd
+  }
+}));
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  };
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ];
+  }
+  return config;
+};
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        '@babel/preset-env'
+      ],
+      plugins: [
+        '@babel/plugin-proposal-class-properties'
+      ]
+    }
+  }];
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+  return loaders;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    hot: true,
+  },
+  devtool: isDev ? 'source-map' : false,
+  plugins: [
+    new HtmlWebpackPugPlugin({
+      adjustIndent: true
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'src/theme/favicon.png'),
+        to: path.resolve(__dirname, 'dist')
+      }]
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ].concat(htmlPlugins),
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: [{
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  autoprefixer({
+                    browsers: ['last 2 versions']
+                  }),
+                ];
+              },
+            },
+          },
+          'less-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              syntax: postcss,
+              plugins: function () {
+                return [
+                  stylelint(),
+                  doiuse({
+                    browsers: ['ie >= 9', 'last 2 versions'],
+                    ignore: ['flexbox', 'rem', 'css-resize', 'css-masks', 'object-fit'],
+                    ignoreFiles: ['**/normalize.css'],
+                    onFeatureUsage: function (usageInfo) {
+                      console.log(usageInfo.message);
+                    }
+                  })
+                ];
+              },
+            },
+          },
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders()
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      }
+    ]
+  }
+};
