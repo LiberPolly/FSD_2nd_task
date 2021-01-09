e02a5de656711a6681a1eef7336175e0beaee7c8
Alina Galochkina <bashkirovaalina@gmail.com>
2021-01-09 23:17:23 +0300

    Initialized new component subscription-text-field.

---
+++ src/components/subscription-text-field/subscription-text-field.js
@@ -0,0 +1,6 @@
+import './subscription-text-field.css';
+// import '@fortawesome/fontawesome-free/js/brands';
+// import '@fortawesome/fontawesome-free/js/solid';
+// import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/all';
+import '@fortawesome/fontawesome-free/css/all.css';
