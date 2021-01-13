ca1acc69ff1f7ff214495d8ae220c90f606292b7
Alina Galochkina <bashkirovaalina@gmail.com>
2021-01-13 14:39:03 +0300

    Styles corrected.

--- src/components/subscription-text-field/subscription-text-field.js
+++ src/components/subscription-text-field/subscription-text-field.js
@@ -1,6 +1,1 @@
 import './subscription-text-field.css';
-// import '@fortawesome/fontawesome-free/js/brands';
-// import '@fortawesome/fontawesome-free/js/solid';
-// import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/all';
-import '@fortawesome/fontawesome-free/css/all.css';
