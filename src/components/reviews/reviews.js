b5b4b1438bce2fb4d60bfaf89e0fcddb63e331b5
Alina Galochkina <bashkirovaalina@gmail.com>
2021-02-08 17:01:11 +0300

    Component edited using tag <template>.

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -4,26 +4,16 @@ import reviews from './reviews.json';
 
 window.onload = () => {
   const reviewsList = document.querySelector('.reviews__list');
+  const template = reviewsList.querySelector('.reviews__template');
 
+  // Для каждого свойства из объекта JSON (./reviews.json) создается шаблон отзыва
   forEach(reviews, (review) => {
-    const template = `
-      <div class="review">
-        <div class="review__container-img">
-          <div style="background-image: url('src/components/reviews/images/${review.foto}.png')" class="review__foto"></div>
-          <div class="review__likes">
-            <label class="like-button">
-              <input class="like-button__checkbox" type="checkbox"/>
-              <span class="like-button__value">${review.likes}</span>
-            </label>
-          </div>
-        </div>
-        <div class="review__container-text">
-          <span class="review__author">${review.name}</span>
-          <span class="review__date">${review.date}</span>
-          <span class="review__text">${review.review}</span>
-        </div>
-      </div>
-    `;
-    reviewsList.insertAdjacentHTML('beforeend', template);
+    template.content.querySelector('.review__foto').style.backgroundImage = `url(src/components/reviews/images/${review.foto}.png)`;
+    template.content.querySelector('.like-button__value').textContent = review.likes;
+    template.content.querySelector('.review__author').textContent = review.name;
+    template.content.querySelector('.review__date').textContent = review.date;
+    template.content.querySelector('.review__text').textContent = review.review;
+    // Клонируется контент тега template с данными текущего отзыва и вставляется в конец списка
+    reviewsList.append(template.content.cloneNode(true));
   });
 };
