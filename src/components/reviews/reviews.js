05b71f24b78ab692174df1490d416585e981d684
Alina Galochkina <bashkirovaalina@gmail.com>
2021-02-14 13:29:20 +0300

    Path to images changed.

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -2,13 +2,12 @@ import './reviews.css';
 import forEach from 'lodash/forEach';
 import reviews from './reviews.json';
 
-window.onload = () => {
+if (document.querySelector('.reviews')) {
   const reviewsList = document.querySelector('.reviews__list');
   const template = reviewsList.querySelector('.reviews__template');
-
   // Для каждого свойства из объекта JSON (./reviews.json) создается шаблон отзыва
   forEach(reviews, (review) => {
-    template.content.querySelector('.review__foto').style.backgroundImage = `url(src/components/reviews/images/${review.foto}.png)`;
+    template.content.querySelector('.review__foto').style.backgroundImage = `url(./images/${review.foto}.png)`;
     template.content.querySelector('.like-button__value').textContent = review.likes;
     template.content.querySelector('.review__author').textContent = review.name;
     template.content.querySelector('.review__date').textContent = review.date;
@@ -16,4 +15,4 @@ window.onload = () => {
     // Клонируется контент тега template с данными текущего отзыва и вставляется в конец списка
     reviewsList.append(template.content.cloneNode(true));
   });
-};
+}
