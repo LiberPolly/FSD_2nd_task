f719f2ca1fa4aaf9ce40dfcc9a936220299a0b1c
Alina Galochkina <bashkirovaalina@gmail.com>
2021-02-14 13:13:56 +0300

    Removed mistake in console.

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -3,40 +3,42 @@ import 'paginationjs/dist/pagination.css';
 import $ from 'jquery';
 import 'paginationjs';
 
-$(() => {
-  $('#pagination-container').pagination({
-    dataSource: [
-      { name: 'hello1' },
-      { name: 'hello2' },
-      { name: 'hello3' },
-      { name: 'hello4' },
-      { name: 'hello5' },
-      { name: 'hello6' },
-      { name: 'hello7' },
-      { name: 'hello8' },
-      { name: 'hello9' },
-      { name: 'hello10' },
-      { name: 'hello11' },
-      { name: 'hello12' },
-      { name: 'hello13' },
-      { name: 'hello14' },
-      { name: 'hello15' },
-    ],
-    pageSize: 1,
-    // callback: (data) => {
-    //   let dataHtml = '<ul>';
-    //   $.each(data, (index, item) => {
-    //     dataHtml += `<li> ${item.name} </li>`;
-    //   });
-    //   dataHtml += '</ul>';
-    //   $('#data-container').html(dataHtml);
-    // },
-    autoHidePrevious: true,
-    autoHideNext: true,
-    pageRange: 1,
-    showNavigator: true,
-    formatNavigator: '<span class="pagination__navigator"><%= currentPage %> – <%= totalPage %> из 100+ вариантов аренды</span>',
-    prevText: '<i class="pagination__arrow material-icons">arrow_back</i>',
-    nextText: '<i class="pagination__arrow material-icons">arrow_forward</i>',
+if (document.querySelector('.pagination')) {
+  $(() => {
+    $('#pagination-container').pagination({
+      dataSource: [
+        { name: 'hello1' },
+        { name: 'hello2' },
+        { name: 'hello3' },
+        { name: 'hello4' },
+        { name: 'hello5' },
+        { name: 'hello6' },
+        { name: 'hello7' },
+        { name: 'hello8' },
+        { name: 'hello9' },
+        { name: 'hello10' },
+        { name: 'hello11' },
+        { name: 'hello12' },
+        { name: 'hello13' },
+        { name: 'hello14' },
+        { name: 'hello15' },
+      ],
+      pageSize: 1,
+      // callback: (data) => {
+      //   let dataHtml = '<ul>';
+      //   $.each(data, (index, item) => {
+      //     dataHtml += `<li> ${item.name} </li>`;
+      //   });
+      //   dataHtml += '</ul>';
+      //   $('#data-container').html(dataHtml);
+      // },
+      autoHidePrevious: true,
+      autoHideNext: true,
+      pageRange: 1,
+      showNavigator: true,
+      formatNavigator: '<span class="pagination__navigator"><%= currentPage %> – <%= totalPage %> из 100+ вариантов аренды</span>',
+      prevText: '<i class="pagination__arrow material-icons">arrow_back</i>',
+      nextText: '<i class="pagination__arrow material-icons">arrow_forward</i>',
+    });
   });
-});
+}
