81f91048412b293d3fdb01b7d0a1029e62ce6635
Alina Galochkina <bashkirovaalina@gmail.com>
2021-02-15 21:11:42 +0300

    Reviews component edited.

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -3,16 +3,18 @@ import forEach from 'lodash/forEach';
 import reviews from './reviews.json';
 
 if (document.querySelector('.reviews')) {
-  const reviewsList = document.querySelector('.reviews__list');
-  const template = reviewsList.querySelector('.reviews__template');
-  // Для каждого свойства из объекта JSON (./reviews.json) создается шаблон отзыва
-  forEach(reviews, (review) => {
-    template.content.querySelector('.review__foto').style.backgroundImage = `url(./images/${review.foto}.png)`;
-    template.content.querySelector('.like-button__value').textContent = review.likes;
-    template.content.querySelector('.review__author').textContent = review.name;
-    template.content.querySelector('.review__date').textContent = review.date;
-    template.content.querySelector('.review__text').textContent = review.review;
-    // Клонируется контент тега template с данными текущего отзыва и вставляется в конец списка
-    reviewsList.append(template.content.cloneNode(true));
-  });
+  window.onload = function createReviewsList() {
+    const reviewsList = document.querySelector('.reviews__list');
+    const template = reviewsList.querySelector('.reviews__template');
+    // Для каждого свойства из объекта JSON (./reviews.json) создается шаблон отзыва
+    forEach(reviews, (review) => {
+      template.content.querySelector('.review__foto').style.backgroundImage = `url(./images/${review.foto}.png)`;
+      template.content.querySelector('.like-button__value').textContent = review.likes;
+      template.content.querySelector('.review__author').textContent = review.name;
+      template.content.querySelector('.review__date').textContent = review.date;
+      template.content.querySelector('.review__text').textContent = review.review;
+      // Клонируется контент тега template с данными текущего отзыва и вставляется в конец списка
+      reviewsList.append(template.content.cloneNode(true));
+    });
+  };
 }
