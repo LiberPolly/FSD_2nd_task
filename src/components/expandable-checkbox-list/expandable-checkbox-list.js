c9f0dfc9cad1bda7ed3846c03fcc5d95841deee1
Alina Galochkina <bashkirovaalina@gmail.com>
2021-02-15 20:52:27 +0300

    Component updated because of using global styles of details tag.

--- src/components/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/components/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,18 +1,1 @@
 import './expandable-checkbox-list.css';
-import forEach from 'lodash/forEach';
-
-const expandableCheckboxListButtons = document.querySelectorAll('.expandable-checkbox-list__button');
-
-function togglExpandableCheckboxListArrow(button) {
-  const details = button.parentElement;
-
-  if (!details.open) {
-    button.classList.remove('expandable-checkbox-list__button_disabled');
-  } else {
-    button.classList.add('expandable-checkbox-list__button_disabled');
-  }
-}
-
-forEach(expandableCheckboxListButtons, ((button) => {
-  button.addEventListener('click', () => togglExpandableCheckboxListArrow(button));
-}));
